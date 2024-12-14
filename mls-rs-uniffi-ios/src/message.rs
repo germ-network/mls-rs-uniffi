91d4ce15144f50958406e89ebc63924d81e74772
Mark @ Germ <mark@germ.network>
2024-12-13 16:51:49 -0800

    add proposal object with API's stubbed out

--- mls-rs-uniffi-ios/src/message.rs
+++ mls-rs-uniffi-ios/src/message.rs
@@ -1,4 +1,6 @@
-// A [`mls_rs::MlsMessage`] wrapper.
+use std::sync::Arc;
+
+use crate::config::SigningIdentityFFI;
 use crate::MlSrsError;
 use mls_rs::error::{IntoAnyError, MlsError};
 
@@ -7,6 +9,7 @@ pub struct Message {
     inner: mls_rs::MlsMessage,
 }
 
+// A [`mls_rs::MlsMessage`] wrapper.
 #[uniffi::export]
 impl Message {
     #[uniffi::constructor]
@@ -32,6 +35,7 @@ impl Message {
         self.inner.epoch()
     }
 
+    //seems unused for now
     // pub fn private_message_content_type(&self) -> Option<u8> {
     //     self.inner.private_message_content_type()
     // }
@@ -41,4 +45,39 @@ impl From<mls_rs::MlsMessage> for Message {
     fn from(inner: mls_rs::MlsMessage) -> Self {
         Self { inner }
     }
+}
+
+#[derive(Clone, Debug, uniffi::Object)]
+pub struct Proposal {
+    _inner: mls_rs::group::proposal::Proposal,
+}
+
+impl From<mls_rs::group::proposal::Proposal> for Proposal {
+    fn from(inner: mls_rs::group::proposal::Proposal) -> Self {
+        Self { _inner: inner }
+    }
+}
+
+#[uniffi::export]
+impl Proposal {
+    //currently unused
+    // pub fn proposal_type(&self) -> u16 {
+    //     self._inner.proposal_type().raw_value()
+    // }
+
+    // pub fn signing_identity(&self) -> Option<Arc<SigningIdentityFFI>> {
+    //     self._inner.signing_identity()
+    //         .map(|s| Arc::new(s.into()))
+    // }
+
+    // pub fn to_bytes(&self) -> Result<Vec<u8>, MlSrsError> {
+    //     Ok(self._inner.mls_encode_to_vec()?)
+    // }
+
+    // pub fn update_bytes(&self) -> Result<Option<Vec<u8>>, MlSrsError> {
+    //     match self._inner.clone() {
+    //         mls_rs::group::proposal::Proposal::Update(update) => Ok(Some(update.mls_encode_to_vec()?)),
+    //         _ => Ok(None)
+    //     }
+    // }
 }
